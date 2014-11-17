d4038ddaf3e4ca5c6d4937418a0e3182968e1f7c
javier baos mora <jbaos@iota26.aulas.gsyc.es>
2014-11-17 09:43:48 +0100

    cliente practicamente finalizado

--- client.py
+++ client.py
@@ -5,15 +5,23 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+entrada = sys.argv
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+if len(entrada) != 4:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
+#Cuando no encuentra puerto Error: No server listening at 193.147.73.20 port 5555 ***Mirar excepcion
+
+METODO = entrada[3]
+direccion = entrada[4].split('@')
+LOGIN_SERVER = direccion[0]
+xxx = direccion[1].split(':')
+IP_SERVER = xxx[0]
+PUERTO_SERVER = xxx[1]
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+LINE = METODO + ' sip:' + LOGIN_SERVER + '@' + IP_SERVER + ' SIP/2.0' + '\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
