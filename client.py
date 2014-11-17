7912d684c2423a91818f1da240b5b00de62767f2
javier baos mora <jbaos@iota17.aulas.gsyc.es>
2014-11-17 15:40:40 +0100

    falta probar con la cancion

--- client.py
+++ client.py
@@ -12,21 +12,17 @@ entrada = sys.argv
 if len(entrada) != 3:
     sys.exit("Usage: python client.py method receiver@IP:SIPport")
 
-#Cuando no encuentra puerto Error: No server listening at 193.147.73.20 port 5555 ***Mirar excepcion
-
 METODO = entrada[1]
-direccion = entrada[2].split('@')
-LOGIN_SERVER = direccion[0]
-xxx = direccion[1].split(':')
-IP_SERVER = xxx[0]
-PUERTO_SERVER = int(xxx[1])
+DIRECCION = entrada[2].split('@')
+LOGIN_SERVER = DIRECCION[0]
+IP_PUERTO = DIRECCION[1].split(':')
+IP_SERVER = IP_PUERTO[0]
+PUERTO_SERVER = int(IP_PUERTO[1])
 
-# Contenido que vamos a enviar
 LINE = METODO + ' sip:' + LOGIN_SERVER + '@' + IP_SERVER + ' SIP/2.0' + '\r\n'
 
 try:
 
-    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((IP_SERVER, PUERTO_SERVER))
@@ -39,19 +35,19 @@ try:
     sentencia = 'SIP/2.0 100 Trying\r\n\r\n'
     sentencia += 'SIP/2.0 180 Ring\r\n\r\n'
     sentencia += 'SIP/2.0 200 OK\r\n\r\n'
-    
+
     if data == sentencia:
-        LINE = 'ACK sip:' + LOGIN_SERVER + '@' + IP_SERVER + ' SIP/2.0' + '\r\n'
-        ACK sip:receptor@IP SIP/2.0
+        LINE = 'ACK' + ' sip:' + LOGIN_SERVER + '@' + IP_SERVER + ' SIP/2.0' + '\r\n'
+        print 'Enviando: ' + LINE
         my_socket.send(LINE + '\r\n')
         data = my_socket.recv(1024)
         print 'Recibido -- ', data
-         
+
     print "Terminando socket..."
 
-    # Cerramos todo
     my_socket.close()
     print "Fin."
+
 except socket.error:
+
     print 'Error: No server listening at' + IP_SERVER + 'port' + PUERTO_SERVER
-    
