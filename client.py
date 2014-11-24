138adeba4aa2680e1d4d29539c875af7273d0333
javier baos mora <jbaos@iota30.aulas.gsyc.es>
2014-11-24 15:13:27 +0100

    pep8 pasado

--- client.py
+++ client.py
@@ -12,12 +12,12 @@ entrada = sys.argv
 if len(entrada) != 3:
     sys.exit("Usage: python client.py method receiver@IP:SIPport")
 
-METODO = entrada[1]
+METODO = entrada[1].upper()
 DIRECCION = entrada[2].split('@')
 LOGIN_SERVER = DIRECCION[0]
-IP_PUERTO = DIRECCION[1].split(':')
-IP_SERVER = IP_PUERTO[0]
-PUERTO_SERVER = int(IP_PUERTO[1])
+IP_PORT = DIRECCION[1].split(':')
+IP_SERVER = IP_PORT[0]
+PORT_SERVER = int(IP_PORT[1])
 
 LINE = METODO + ' sip:' + LOGIN_SERVER + '@' + IP_SERVER + ' SIP/2.0' + '\r\n'
 
@@ -25,7 +25,7 @@ try:
 
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((IP_SERVER, PUERTO_SERVER))
+    my_socket.connect((IP_SERVER, PORT_SERVER))
 
     print "Enviando: " + LINE
     my_socket.send(LINE + '\r\n')
@@ -37,7 +37,8 @@ try:
     sentencia += 'SIP/2.0 200 OK\r\n\r\n'
 
     if data == sentencia:
-        LINE = 'ACK' + ' sip:' + LOGIN_SERVER + '@' + IP_SERVER + ' SIP/2.0' + '\r\n'
+        LINE = 'ACK' + ' sip:' + LOGIN_SERVER + '@' + IP_SERVER
+        LINE += ' SIP/2.0' + '\r\n'
         print 'Enviando: ' + LINE
         my_socket.send(LINE + '\r\n')
         data = my_socket.recv(1024)
@@ -49,5 +50,5 @@ try:
     print "Fin."
 
 except socket.error:
-
-    print 'Error: No server listening at ' + IP_SERVER + ' port ' + str(PUERTO_SERVER)
+    PORT = str(PORT_SERVER)
+    print 'Error: No server listening at ' + IP_SERVER + ' port ' + PORT
